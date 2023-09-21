8a58d537c57918255a5580612b5af357d0f79970
Jatin Varshney <iamjatinvarshney@gmail.com>
2023-09-21 15:08:31 +0530

    Serializing Objects

--- store/views.py
+++ store/views.py
@@ -1,13 +1,37 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import get_object_or_404
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from .models import Product
+from .serializers import ProductSerializer
 
 # Product List View
 @api_view()
 def product_list(request):
-    return Response("Product List View Working Fine")
+    queryset = Product.objects.all()
+    """ 
+    - When you have a queryset or a list of objects, you need to specify many=True to tell the serializer that you are dealing with multiple objects.
+    """
+    serializer = ProductSerializer(queryset, many=True)
+    return Response(serializer.data)
 
 # Product Detail View
 @api_view()
 def product_detail(request, id):
-    return Response(f"Product Detail View Working Fine - Product ID: {id}")
+    product = get_object_or_404(Product, id=id)
+    serializer = ProductSerializer(product)
+    return Response(serializer.data)
+
+    """
+    - Error Handling and 404 Status Code: get_object_or_404 is a Django shortcut function commonly used to retrieve a single object from the database, and it raises a Http404 exception if the object is not found. This is a convenient way to handle the case where an object does not exist because it automatically triggers the 404 error response, which is a standard way to indicate to clients that the requested resource was not found.
+    """
+ 
+    """
+    - Handling the data with try-except block :
+
+        try:
+            product = Product.objects.get(id=id)
+            serializer = ProductSerializer(product)
+            return Response(serializer.data)
+        except product.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+    """
