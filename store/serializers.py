6028e50c7179f9733f3cb7402671d6f3af52228e
Jatin Varshney <iamjatinvarshney@gmail.com>
2023-09-21 16:39:09 +0530

    Model Serializers

--- store/serializers.py
+++ store/serializers.py
@@ -3,28 +3,32 @@ from .models import Product, Collection
 from decimal import Decimal
 
 # Collection Serializer
-class CollectionSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    title = serializers.CharField(max_length=255)
+class CollectionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Collection
+        fields = ['id', 'title']
+
+    """
+    - A Model Serializer in Django REST framework offers code optimization by automatically generating serialization fields based on the structure of a model. It eliminates the need to define each field manually, saving developers time and reducing redundancy. While Model Serializers can automatically include all fields from the model, developers have the flexibility to customize which fields are exposed in the serialized output. This customization ensures that only the necessary data is exposed, enhancing security and performance in API development.
+    """
 
 # Product Serializer
-class ProductSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    title = serializers.CharField(max_length=255)
-    unit_price = serializers.DecimalField(max_digits=6, decimal_places=2)
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ['id', 'title', 'price', 'price_with_tax', 'collection']
 
+    """ 
+    - source = "unit_price": This indicates that when you serialize this field, you should use the value from the unit_price attribute of the model. In other words, you're telling the serializer to look for the value in the unit_price attribute and use it when serializing the price field.
+    """
+    price = serializers.DecimalField(max_digits=6, decimal_places=2, source="unit_price")
+    price_with_tax = serializers.SerializerMethodField(method_name='calculate_tax') 
+    
     """
     - 'price_with_tax': This is a custom field you're defining in your serializer. It's not a direct attribute of your model but rather a calculated value that you want to include when serializing an object.
     
     - 'serializers.SerializerMethodField': This field allows you to define a custom method (calculate_tax in this case) to compute the value of the field. It's particularly useful when you need to perform complex calculations or include data that doesn't directly map to a model attribute.
-    """
-    price_with_tax = serializers.SerializerMethodField(method_name='calculate_tax') 
-    collection = serializers.HyperlinkedRelatedField(
-        queryset=Collection.objects.all(),
-        view_name='collection-detail',
-    )
-    
-    """
+
     - 'calculate_tax' Function, we are using in 'price_with_tax' custom serializer field.
     """
     def calculate_tax(self, product:Product):
@@ -33,7 +37,7 @@ class ProductSerializer(serializers.Serializer):
 
     """
     - Serializing Relationships :
-    
+
     1. PrimaryKeyRelatedField: When working with Django models that have foreign key relationships, you can use the PrimaryKeyRelatedField in your serializer to represent these relationships. This field allows you to associate a related object by its primary key (ID). However, it's essential to be aware that when you use PrimaryKeyRelatedField, it returns the primary key (ID) of the related object, rather than its name or title. In this we need to pass an parameter like queryset.
     
     Code:
