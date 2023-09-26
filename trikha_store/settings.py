64cfa98f6b46350d5936eb0d23a25a9e8a0c2685
Jatin Varshney <iamjatinvarshney@gmail.com>
2023-09-26 16:51:48 +0530

    Adding the Authentication Endpoints

--- trikha_store/settings.py
+++ trikha_store/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'djoser',
     'django_filters',
     'playground',
     'store',
@@ -134,6 +135,13 @@ REST_FRAMEWORK = {
     'COERCE_DECIMAL_TO_STRING': False,
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+}
+
+SIMPLE_JWT = {
+   'AUTH_HEADER_TYPES': ('JWT',),
 }
 
 AUTH_USER_MODEL = 'core.User'
--- trikha_store/urls.py
+++ trikha_store/urls.py
@@ -12,5 +12,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('playground.urls')),
     path('store/', include('store.urls')),
+    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.jwt')),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
