44a2ddc00b6e5c2168159fde0f28c001a9da07b9
Jatin Varshney <iamjatinvarshney@gmail.com>
2023-09-26 12:22:38 +0530

    Cart - Deleting a Cart Item

--- store/serializers.py
+++ store/serializers.py
@@ -122,7 +122,8 @@ class AddCartItemSerializer(serializers.ModelSerializer):
         cart_id = self.context['cart_id']
         product_id = self.validated_data['product_id']
         quantity = self.validated_data['quantity']
-
+        
+        # Saving the product with try-except block 
         try:
             cart_item = CartItem.objects.get(cart_id=cart_id, product_id=product_id)
             cart_item.quantity += quantity
